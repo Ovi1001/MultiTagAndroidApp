26db5d4db7f08a42ac2fcb4966f32c19ed1af35b
Ovi1001 <ovi26470@gmail.com>
2022-05-27 21:42:06 -0700

    feat: To SignInScreen button works as intended. Back button to homeScreen from SignInScreen works as intended.

--- app/src/main/java/com/example/multitagandroidapp/MainActivity.java
+++ app/src/main/java/com/example/multitagandroidapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.multitagandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureSignInScreenBtn();
+    }
+
+    public void configureSignInScreenBtn()
+    {
+        Button signInBtn = (Button) findViewById(R.id.signInScreenBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, SignInScreen.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/multitagandroidapp/SignInScreen.java
+++ app/src/main/java/com/example/multitagandroidapp/SignInScreen.java
@@ -3,12 +3,27 @@ package com.example.multitagandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInScreen extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+        configureBackFromSignInBtn();
+    }
+
+    private void configureBackFromSignInBtn()
+    {
+        Button signInbackButton = (Button) findViewById(R.id.backFromSignInBtn);
+        signInbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
