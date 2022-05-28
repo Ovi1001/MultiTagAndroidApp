dbde228244bf56e7290a33b5f19a9c679749662c
Ovi1001 <ovi26470@gmail.com>
2022-05-28 15:00:12 -0700

    feat: Added a system of validation when creating an account. Checks to see if username, email, and password meet a set of requirements

--- app/src/main/java/com/example/multitagandroidapp/CreateAccountScreen.java
+++ app/src/main/java/com/example/multitagandroidapp/CreateAccountScreen.java
@@ -3,6 +3,7 @@ package com.example.multitagandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,10 +12,13 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class CreateAccountScreen extends AppCompatActivity {
+import java.util.regex.Pattern;
+
+public class CreateAccountScreen extends AppCompatActivity
+{
 
     //private FirebaseAuth mAuth;
-    //private EditText editTextEmail, editTextUsername, editTextPassword;
+    private EditText editTextEmail, editTextUsername, editTextPassword;
     private ProgressBar progressBar;
 
     @Override
@@ -26,6 +30,11 @@ public class CreateAccountScreen extends AppCompatActivity {
         configureCreateAccBtn();
 
         //mAuth = FirebaseAuth.getInstance();
+
+        editTextUsername = (EditText) findViewById(R.id.createAccUsernameInput);
+        editTextEmail = (EditText) findViewById(R.id.createAccEmailInput);
+        editTextPassword = (EditText) findViewById(R.id.createAccPasswordInput);
+
         progressBar = findViewById(R.id.createAccProgressBar);
     }
 
@@ -46,17 +55,103 @@ public class CreateAccountScreen extends AppCompatActivity {
         createAccBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //registerUser();
+                registerUser();
                 progressBar.setVisibility(View.VISIBLE);
             }
         });
     }
 
-//    private void registerUser()
-//    {
-//        String email = ((EditText) findViewById(R.id.createAccUsernameInput)).getText().toString().trim(); // Getting email provided from text input
-//        String password = ((EditText) findViewById(R.id.createAccPasswordInput)).getText().toString().trim(); // Getting password provided from text input
-//    }
+    private void registerUser()
+    {
+        String username = editTextUsername.getText().toString().trim(); // Getting username provided from text input
+        String email = editTextEmail.getText().toString().trim(); // Getting email provided from text input
+        String password = editTextPassword.getText().toString().trim(); // Getting password provided from text input
 
+        //Username validation
+        Pattern space = Pattern.compile("\\s");
+        Pattern numeric = Pattern.compile("\\D");
 
-}
+        if (username.isEmpty())
+        {
+            editTextUsername.setError("A username is required!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if (username.length() < 3)
+        {
+            editTextUsername.setError("Username has to be at LEAST 3 characters!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if (!numeric.matcher(username).find()) //Checks for a non digit character
+        {
+            editTextUsername.setError("A username cannot be numbers!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if (space.matcher(username).find()) // checks for a white space character
+        {
+            editTextUsername.setError("A username cannot have white spaces!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        //Email validation
+        if (email.isEmpty())
+        {
+            editTextEmail.setError("An email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) // checks for a good email
+        {
+            editTextEmail.setError("no way jose. provide a good email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        //Password validation
+        Pattern uppercase = Pattern.compile("[A-Z]");
+        Pattern digit = Pattern.compile("[0-9]");
+
+        if (password.isEmpty())
+        {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6)
+        {
+            editTextPassword.setError("Password has to at LEAST be 6 characters! duh");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (!uppercase.matcher(password).find())
+        {
+            editTextPassword.setError("Password has to include at LEAST 1 capital letter!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (!digit.matcher(password).find())
+        {
+            editTextPassword.setError("Password has to include at LEAST 1 number!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (space.matcher(password).find())
+        {
+            editTextPassword.setError("A password cannot have white spaces!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+    }//end of registerUser method
+}//end of CreateAccountScreen class
